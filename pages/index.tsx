0b5cfc922995a29aba0973579b3b725b240c5a75
Sara Acevedo <saraacevedo@transfix.io>
2025-11-12 16:29:34 -0500

    adjusting orderby field

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       repoName: selectedRepo,
       first: 100,
       orderBy: {
-        field: IIssueOrderField.CREATED_AT,
+        field: IIssueOrderField.UPDATED_AT,
         direction: IOrderDirection.DESC
       },
       pullRequestStates: [IPullRequestState.OPEN]
